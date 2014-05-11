51a3367b587bdf500797c9132695339916035b59
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2014-05-11 13:18:24 +0200

    First basic version. The user can add tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,15 +4,15 @@
  */
 
 exports.list = function(req, res){
-    var title = 'Top 10 NBA Crossovers of the 2013-2014 Regular Season';
+    /*var title = 'Top 10 NBA Crossovers of the 2013-2014 Regular Season';
     var url = 'https://www.youtube.com/watch?v=RhyZrDKejBo';
     var author = 'Guillaume';
     var sample = [
         {title : title, url : url, author : author, creation_datetime : new Date()},
         {title : title, url : url, author : author, creation_datetime : new Date()},
-        {title : title, url : url, author : author, creation_datetime : new Date()}];
+        {title : title, url : url, author : author, creation_datetime : new Date()}];*/
 
-    res.render('tasks', { title: 'Dashboard', tasks: sample});
+    res.render('tasks', { title: 'Dashboard', tasks: []});
 
 };
 
